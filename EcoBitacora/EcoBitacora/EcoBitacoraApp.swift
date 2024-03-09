e2ea4061aa7e0db40770d6a768faf38b6cca0bf3
Sebastian Rosas Maciel <rosassebastian2003@gmail.com>
2024-03-09 15:03:20 -0600

    Project created

---
+++ EcoBitacora/EcoBitacora/EcoBitacoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoBitacoraApp.swift
+//  EcoBitacora
+//
+//  Created by Sebastian Rosas Maciel on 3/9/24.
+//
+
+import SwiftUI
+
+@main
+struct EcoBitacoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
